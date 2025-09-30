0bacaaef10200c1776f94232e0140a4562ac0e84
hh.bertram82@gmail.com <hh.bertram82@gmail.com>
2025-09-30 13:49:02 +0000

    corrections

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,30 @@
+with 
+
+sales as (
+
+    select 
+       orders_id,
+       pdt_id AS products_id,
+       quantity,
+       revenue
+    from {{ source('raw', 'sales') }} 
+
+),
+
+product as (
+
+    select
+        products_id,
+        SAFE_CAST(purchSE_PRICE AS INT64) AS purchase_price
+    from {{ source('raw', 'product') }} 
+
+)
+select
+     s.orders_id,
+     s.products_id,
+     s.quantity,
+     s.revenue,
+     (s.quantity * p.purchase_price) AS purchase_cost,
+     (s.revenue - (s.quantity * p.purchase_price)) AS margin
+from sales s      
+left join product p using (products_id)
