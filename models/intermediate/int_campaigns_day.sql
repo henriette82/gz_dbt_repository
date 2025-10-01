cb96a3c0934782a457913319ddf2038fee48b7e7
hh.bertram82@gmail.com <hh.bertram82@gmail.com>
2025-10-01 14:48:36 +0000

    corrections

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,9 +1,26 @@
-select
-     date_date,
-     SUM(ads_cost) AS ads_cost,
-     SUM(impression) AS impression,
-     SUM(click) AS click
-
+WITH ads_per_day AS (
+    
+   select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaign_name,
+        ads_cost,
+        impression,
+        click 
 from {{ ref('int_campaigns') }}
+
+),
+agg AS (
+    select
+     date_date,
+     COUNT(distinct campaign_key) AS count_campaign_key,
+     SUM(ads_cost) AS ads_cost,
+     SUM(impression) AS ads_impression,
+     SUM(click) AS ads_click
+from ads_per_day
 GROUP BY date_date
+)
+select*
+from agg
 ORDER BY date_date DESC
