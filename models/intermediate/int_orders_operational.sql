ccc9847f7b5b0bd533e7bbbba868bc4fe2f6ea27
hh.bertram82@gmail.com <hh.bertram82@gmail.com>
2025-10-01 07:43:43 +0000

    corrections

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,14 +2,14 @@ select
     o.orders_id,
     o.date_date,
     round(
-        o.margin + s.shipping_fee - (s.logcost + s.ship_cost), 2
+        o.margin + s.shipping_fee - (s.log_cost + s.ship_cost), 2
     ) as operational_margin,
     o.quantity,
     o.revenue,
     o.purchase_cost,
     o.margin,
     s.shipping_fee,
-    s.logcost as log_cost,
+    s.log_cost,
     s.ship_cost
 from {{ ref("int_orders_margin") }} o
 left join {{ ref("stg_raw__ship") }} s using (orders_id)
